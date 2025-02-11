4d5b3b26c408d6a6c2fcbadeedbca5a9946bc772
Maciej Pankiewicz <bukkapc@gmail.com>
2025-02-11 14:34:17 +0100

    added last set of tests

--- tests/page-objects/history.page.ts
+++ tests/page-objects/history.page.ts
@@ -1,5 +1,19 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class HistoryPage {
-  constructor(private page: Page) {}
+  goToGameStartButton: Locator;
+  goToMoveButton: Locator;
+  turnHistoryLabelText: Locator;
+  gotoMoveButton8: Locator;
+  constructor(private page: Page) {
+    this.goToGameStartButton = page.getByRole("button", {
+      name: "Go to game start",
+    });
+    this.goToMoveButton = page.getByRole("button", {
+      name: "Go to move # move",
+    });
+    this.turnHistoryLabelText = page.getByText("History");
+
+    this.gotoMoveButton8 = page.locator("li:nth-child(8) > button");
+  }
 }
--- tests/tictactoe.spec.ts
+++ tests/tictactoe.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { BoardPage } from "./page-objects/board.page";
+import { HistoryPage } from "./page-objects/history.page";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
@@ -152,9 +153,47 @@ test.describe("Check RESET button funcionality and display of next Player", () =
     expect(await newBoard.gameStatusText.innerText()).toBe("Next player: X");
   });
 });
+test.describe("Check history of turns", () => {
+  test("Check if Go to game start button works ", async ({ page }) => {
+    // ARRANGE
+    const newHistory = new HistoryPage(page);
+    const newBoard = new BoardPage(page);
+    await expect(newHistory.turnHistoryLabelText).toBeVisible();
+    await expect(newHistory.goToGameStartButton).toBeVisible();
+    await newBoard.firstSquare.click();
+    expect(await newBoard.gameStatusText.innerText()).toBe("Next player: O");
+    await newHistory.goToGameStartButton.click();
+    expect(await newBoard.gameStatusText.innerText()).toBe("Next player: X");
+    await newBoard.checkEachRowForSelectedSquares();
+  });
+  test("Check if Turn History reacts to player moves ", async ({ page }) => {
+    // ARRANGE
+    const newHistory = new HistoryPage(page);
+    const newBoard = new BoardPage(page);
 
-// check history of the turns
-// check if possible to go back to any previous turn
-// go to game start from history
+    await expect(newHistory.goToGameStartButton).toBeVisible();
+    await newBoard.firstSquare.click();
+    expect(await newHistory.goToMoveButton.count()).toBe(1);
+    await newBoard.secondSquare.click();
+    expect(await newHistory.goToMoveButton.count()).toBe(2);
+    await newHistory.goToGameStartButton.click();
+    await newBoard.firstSquare.click();
+    expect(await newHistory.goToMoveButton.count()).toBe(1);
+    await newBoard.resetGame();
+    expect(await newHistory.goToMoveButton.count()).toBe(0);
+  });
+
+  test("Verify Turn History updates Gameboard correctly ", async ({ page }) => {
+    // ARRANGE
+    const newHistory = new HistoryPage(page);
+    const newBoard = new BoardPage(page);
+
+    await newBoard.playersAlmostDrawByAllMoves();
+    await newHistory.gotoMoveButton8.click();
+    expect(await newBoard.gameStatusText.innerText()).toBe("Next player: O");
+    await expect(newBoard.ninthSquare).toHaveText("");
+  });
+});
+
 // ADD BUG REPORT FOR MISSING DRAW MESSAGE
-// fix upstream
+// add ui improvements text color, add numer/whose turn it was to turn history, history label could be bigger, after 1st move and adding of history, board move to the left
