49346991292c5de71e38404c4b93388d9e3d1723
Maciej Pankiewicz <bukkapc@gmail.com>
2025-02-11 11:29:47 +0100

    first test

--- tests/page-objects/board.page.ts
+++ tests/page-objects/board.page.ts
@@ -1,5 +1,56 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class BoardPage {
-  constructor(private page: Page) {}
+  firstSquare: Locator;
+  secondSquare: Locator;
+  thirdSquare: Locator;
+  fourthSquare: Locator;
+  fifthSquare: Locator;
+  sixthSquare: Locator;
+  seventhSquare: Locator;
+  eighthSquare: Locator;
+  ninthSquare: Locator;
+  gameStatusText: Locator;
+  gameResetButton: Locator;
+  constructor(private page: Page) {
+    this.firstSquare = page.locator(".board-row .square").nth(0);
+    this.secondSquare = page.locator(".board-row .square").nth(1);
+    this.thirdSquare = page.locator(".board-row .square").nth(2);
+    this.fourthSquare = page.locator(".board-row .square").nth(3);
+    this.fifthSquare = page.locator(".board-row .square").nth(4);
+    this.sixthSquare = page.locator(".board-row .square").nth(5);
+    this.seventhSquare = page.locator(".board-row .square").nth(6);
+    this.eighthSquare = page.locator(".board-row .square").nth(7);
+    this.ninthSquare = page.locator(".board-row .square").nth(8);
+    this.gameStatusText = page.locator(".status");
+    this.gameResetButton = page.locator(".reset-wrapper");
+  }
+
+  async playerXwinsByRow(): Promise<void> {
+    await this.firstSquare.click();
+    await this.fourthSquare.click();
+    await this.secondSquare.click();
+    await this.fifthSquare.click();
+    await this.thirdSquare.click();
+  }
+  async playerXwinsByColumn() {
+    await this.firstSquare.click();
+    await this.secondSquare.click();
+    await this.fourthSquare.click();
+    await this.fifthSquare.click();
+    await this.seventhSquare.click();
+  }
+  async playerXwinsByDiag() {
+    await this.firstSquare.click();
+    await this.secondSquare.click();
+    await this.fifthSquare.click();
+    await this.sixthSquare.click();
+    await this.ninthSquare.click();
+  }
+  // async playerOwinsByRow() {
+  // }
+  // async playerOwinsByColumn() {
+  // }
+  // async playerOwinsByDiag() {
+  // }
 }
--- tests/tictactoe.spec.ts
+++ tests/tictactoe.spec.ts
@@ -1,10 +1,48 @@
 import { test, expect } from "@playwright/test";
+import { BoardPage } from "./page-objects/board.page";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/");
-  await page.locator(".board-row .square").nth(0).click();
-  await page.locator(".board-row .square").nth(3).click();
-  await page.locator(".board-row .square").nth(2).click();
-  await page.locator(".board-row .square").nth(4).click();
-  await expect(page.locator(".board-row .square").nth(0)).toHaveText("X");
 });
+
+test.describe("Check if the game is working properly when Player X wins", () => {
+  test("Check message when Player X wins by row", async ({ page }) => {
+    // ARRANGE
+    const newBoard = new BoardPage(page);
+    // ACT
+    await newBoard.playerXwinsByRow();
+    // ASSERT
+    expect(await newBoard.gameStatusText.innerText()).toBe("Winner: X");
+  });
+
+  test("Check message when Player X wins by column", async ({ page }) => {
+    // ARRANGE
+    const newBoard = new BoardPage(page);
+    // ACT
+    await newBoard.playerXwinsByColumn();
+    // ASSERT
+    expect(await newBoard.gameStatusText.innerText()).toBe("Winner: X");
+  });
+
+  test("Check message when Player X wins by diagonal", async ({ page }) => {
+    // ARRANGE
+    const newBoard = new BoardPage(page);
+    // ACT
+    await newBoard.playerXwinsByDiag();
+    // ASSERT
+    expect(await newBoard.gameStatusText.innerText()).toBe("Winner: X");
+  });
+});
+// PLayer X wins - check the message
+// PLayer 0 wins - check the message
+// player wins by row
+// player wins by column
+// player wins diagonally
+// draw by all fields - check the message
+// Check if possible to select selected field
+// Check if possible to check field after game is finished
+// check current player turn
+// check reset button in couple scenarios
+// check history of the turns
+// check if possible to go back to any previous turn
+// go to game start from history
